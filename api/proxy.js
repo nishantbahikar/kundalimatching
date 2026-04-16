2493c95f276df91f9903e6feb014b8733fd25dc8
nishantbahikar <nishantbahikar@gmail.com>
2026-04-17 01:11:07 +0530

    Delete api/proxy.js

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  const response = await fetch('/api/proxy', {
-    method: 'POST',
-    body: JSON.stringify(req.body)
-  });
-  const data = await response.json();
-  res.status(200).json(data);
-}
